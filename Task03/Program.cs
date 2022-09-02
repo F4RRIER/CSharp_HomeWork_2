ab3f7b902225dd74e8cab8772b2a5a06bf732ed6
F4RRIER <108473368+F4RRIER@users.noreply.github.com>
2022-09-02 22:01:07 +0300

    Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите цифру, обозначающую день недели");
+int DayNum = int.Parse(Console.ReadLine());
+
+void WeekDay (int DayNum) 
+{
+    if (DayNum == 6 || DayNum == 7) 
+        {
+            System.Console.WriteLine("Это выходной день");
+        }
+    else if (DayNum < 1 || DayNum > 7) 
+        {
+            System.Console.WriteLine("Введите число от 1 до 7");
+        }
+    else System.Console.WriteLine("Это рабочий день");
+}
+WeekDay(DayNum);
