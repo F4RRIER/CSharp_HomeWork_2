6160a58c34cc96a14a4b9ccc8d41e270f01e9c98
F4RRIER <108473368+F4RRIER@users.noreply.github.com>
2022-09-02 22:00:42 +0300

    Task 01

---
+++ Task01/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Введите трёхзначное число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeNumber);
+System.Console.WriteLine("Вторая цифра этого числа: "+stringNumber[1]);
