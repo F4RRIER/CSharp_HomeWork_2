6fcbc2ee3c6775f375843c497b080f526ebdfb90
F4RRIER <108473368+F4RRIER@users.noreply.github.com>
2022-09-02 22:00:55 +0300

    Task 02

---
+++ Task02/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число: ");
+int RandomNum = int.Parse(Console.ReadLine());
+string RandomNumText = Convert.ToString(RandomNum);
+if (RandomNumText.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + RandomNumText[2]);
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
